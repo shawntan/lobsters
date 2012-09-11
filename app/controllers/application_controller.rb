5bc8173744b7a0b7ed84b21627de7f088aa0575f
Eli James <ejames@pandamian.com>
2012-09-11 10:54:54 +0800

    Get rid of @traffic calculator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,35 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user
-  before_filter :increase_traffic_counter
-
-  TRAFFIC_DECREMENTER = 0.15
 
   def authenticate_user
     if session[:u]
       @user = User.find_by_session_token(session[:u])
     end
-
-    true
-  end
-
-  def increase_traffic_counter
-    @traffic = 1.0
-
-    Keystore.transaction do
-      date = (Keystore.value_for("traffic:date") || Time.now.to_i)
-      traffic = (Keystore.incremented_value_for("traffic:hits", 0).
-        to_f / 100.0) + 1.0
-
-      # every second, decrement traffic by some amount
-      @traffic = [ 1.0, traffic.to_f -
-        ((Time.now.to_i - date) * TRAFFIC_DECREMENTER) ].max
-
-      Keystore.put("traffic:date", Time.now.to_i)
-      Keystore.put("traffic:hits", (@traffic * 100.0).to_i)
-    end
-
-    Rails.logger.info "  Traffic level: #{@traffic}"
 
     true
   end
